4185f3ec8e63558c5c38ae3b05c52deb4c272c24
Mukarram Srour <mukarramsrour@gmail.com>
2024-02-04 22:19:37 +0300

    All cases added

--- src/AlmosaferPKG/MyTestCases.java
+++ src/AlmosaferPKG/MyTestCases.java
@@ -1,18 +1,21 @@
 package AlmosaferPKG;
 
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
 import static org.testng.Assert.assertEquals;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Random;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
 public class MyTestCases extends Parameters {
-	
+
 	@BeforeTest
 	public void Setup() {
 		driver.manage().window().maximize();
@@ -20,7 +23,7 @@ public class MyTestCases extends Parameters {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
 		WebElement popUpMsg = driver.findElement(By.cssSelector(".sc-iBmynh.izXFRL"));
-		
+
 		if (popUpMsg.isDisplayed()) {
 			WebElement sarBtn = driver
 					.findElement(By.cssSelector(".sc-jTzLTM.hQpNle.cta__button.cta__saudi.btn.btn-primary"));
@@ -28,69 +31,153 @@ public class MyTestCases extends Parameters {
 		}
 	}
 
-	@Test(enabled = false)
+	@Test(priority = 1)
 	public void LanguageEN() {
 		String actualLanguage = driver.findElement(By.tagName("html")).getAttribute("lang").toUpperCase();
 		String expectedLanguage = "EN";
 		assertEquals(actualLanguage, expectedLanguage);
 	}
 
-	@Test(enabled = false)
+	@Test(priority = 2)
 	public void Currency() {
-		String actualCurrency = driver.findElement(By.xpath("//button[@data-testid='Header__CurrencySelector']")).getText();
+		String actualCurrency = driver.findElement(By.xpath("//button[@data-testid='Header__CurrencySelector']"))
+				.getText();
 		String expectedCurrency = "SAR";
 		assertEquals(actualCurrency, expectedCurrency);
 	}
 
-	@Test(enabled = false)
+	@Test(priority = 3)
 	public void ContactNumber() {
 		String actualPhoneNumber = driver.findElement(By.tagName("footer"))
 				.findElement(By.cssSelector(".sc-dxgOiQ.hTjMfW.col-lg-12")).getText();
 		String expectedPhoneNumber = "920000997";
-		assertEquals(actualPhoneNumber.contains(expectedPhoneNumber), true );
-		
+		assertEquals(actualPhoneNumber.contains(expectedPhoneNumber), true);
+
 		String expectedWtsappNumber = "+966554400000";
 		String wtsappNumber = driver.findElement(By.tagName("strong")).getText();
 		assertEquals(wtsappNumber, expectedWtsappNumber);
 	}
-	
-	@Test(enabled = false)
-	public void QitafLogo () {
-		WebElement qitafLogoInFooter = driver.findElement(By.tagName("footer")).findElement(By.cssSelector(".sc-bdVaJa.bxRSiR.sc-ekulBa.eYboXF"));
+
+	@Test(priority = 4)
+	public void QitafLogo() {
+		WebElement qitafLogoInFooter = driver.findElement(By.tagName("footer"))
+				.findElement(By.cssSelector(".sc-bdVaJa.bxRSiR.sc-ekulBa.eYboXF"));
 		assertEquals(qitafLogoInFooter.isDisplayed(), true);
 	}
-	
-	@Test(enabled = false)
-	public void hotelsTab () {
+
+	@Test(priority = 5)
+	public void hotelsTab() {
 		WebElement hotelsTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
 		boolean hotelsTabSelected = Boolean.parseBoolean(hotelsTab.getAttribute("aria-selected"));
 		assertEquals(hotelsTabSelected, false);
 	}
-	
-	@Test(enabled = false)
+
+	@Test(priority = 6)
 	public void DepartureDate() {
-		String departureDate = driver.findElement(By.xpath("//div[@data-testid='FlightSearchBox__FromDateButton']")).getText();
-		System.out.println(departureDate);
-		System.out.println(LocalDate.now().format(java.time.format.DateTimeFormatter.ofPattern("MMMM dd")));
+		String day = driver
+				.findElement(By.cssSelector("div[class='sc-iHhHRJ sc-kqlzXE blwiEW'] span[class='sc-cPuPxo LiroG']"))
+				.getText();
+		String month = driver
+				.findElement(By.cssSelector("div[class='sc-iHhHRJ sc-kqlzXE blwiEW'] span[class='sc-bYnzgO doqpRk']"))
+				.getText();
+		driver.findElement(By.cssSelector("div[class='sc-iHhHRJ sc-kqlzXE blwiEW'] span[class='sc-cPuPxo LiroG']"))
+				.click();
+		String year = driver.findElement(By.cssSelector(".sc-jDwBTQ.sc-gPEVay.htcLFw")).getText();
+
+		String departureDate = year + " " + month + " " + day;
+		String tomorrowDate = LocalDate.now().plusDays(1)
+				.format(java.time.format.DateTimeFormatter.ofPattern("YYYY MMMM dd"));
+
+		assertEquals(departureDate, tomorrowDate);
 	}
-	
-	@Test
-	public void RandomLanguage () {
+
+	@Test(priority = 7)
+	public void ReturnDate() throws InterruptedException {
+		String day = driver
+				.findElement(By.cssSelector("div[class='sc-iHhHRJ sc-OxbzP edzUwL'] span[class='sc-cPuPxo LiroG']"))
+				.getText();
+		String month = driver
+				.findElement(By.cssSelector("div[class='sc-iHhHRJ sc-OxbzP edzUwL'] span[class='sc-bYnzgO doqpRk']"))
+				.getText();
+		driver.findElement(By.cssSelector("div[class='sc-iHhHRJ sc-OxbzP edzUwL'] span[class='sc-cPuPxo LiroG']"))
+				.click();
+		String year = driver
+				.findElement(By
+						.cssSelector("div[class='sc-cMljjf sc-jAaTju lEldS'] span[class='sc-jDwBTQ sc-gPEVay htcLFw']"))
+				.getText();
+
+		String returnDate = year + " " + month + " " + day;
+		String afterTomorrowDate = LocalDate.now().plusDays(2)
+				.format(java.time.format.DateTimeFormatter.ofPattern("YYYY MMMM dd"));
+
+		assertEquals(returnDate, afterTomorrowDate);
+	}
+
+	@Test(priority = 8)
+	public String RandomLanguage() {
 		Random rnd = new Random();
 		int rndWebsiteLang = rnd.nextInt(websites.length);
-		
+
 		driver.get(websites[rndWebsiteLang]);
-		
+
 		String expectedLang = "";
 		String actualLang = driver.findElement(By.tagName("html")).getAttribute("lang");
-		
-		if(driver.getCurrentUrl().endsWith("ar")) {
+
+		if (driver.getCurrentUrl().endsWith("ar")) {
 			expectedLang = "ar";
 		} else {
-			expectedLang ="en";
+			expectedLang = "en";
 		}
-		
+
 		assertEquals(actualLang, expectedLang);
+		return actualLang;
+	}
+
+	@Test(priority = 9)
+	public void RandomCity() throws InterruptedException {
+		String lang = RandomLanguage();
+		Thread.sleep(2000);
+		driver.findElement(By.id("uncontrolled-tab-example-tab-hotels")).click();
+		WebElement cityInput = driver.findElement(By.xpath("//input[@data-testid='AutoCompleteInput']"));
+		if (lang == "en") {
+			cityInput.sendKeys(citiesEN[rndCitiesEN]);
+		} else {
+			cityInput.sendKeys(citiesAR[rndCitiesAR]);
+
+		}
+		driver.findElement(By.xpath("//ul[@data-testid='AutoCompleteResultsList']"))
+				.findElement(By.xpath("//li[@data-testid='AutoCompleteResultItem0']")).click();
+	}
+
+	@Test(priority = 10)
+	public void RandomRoom() {
+		WebElement selectList = driver.findElement(By.xpath("//select[@data-testid='HotelSearchBox__ReservationSelect_Select']"));
+		Select roomSelector = new Select(selectList);
+		Random rndRoom = new Random();
+		roomSelector.selectByIndex(rndRoom.nextInt(2));
+	}
+	
+	@Test(priority = 11)
+	public void ClickSearchHotels () {
+		WebElement searchBtn = driver.findElement(By.xpath("//button[@data-testid='HotelSearchBox__SearchButton']"));
+		searchBtn.click();
+	}
+	
+	@Test(priority = 12)
+	public void LoadSearchResultPage () throws InterruptedException {
+		Thread.sleep(25000);
+		String loadedMsg = driver.findElement(By.xpath("//span[@data-testid='HotelSearchResult__resultsFoundCount']")).getText();
+
+		assertEquals(Character.isDigit(loadedMsg.charAt(0)), true);
+	}
+	
+	@Test(priority = 13)
+	public void LowestHighestPrices() throws InterruptedException {
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[@data-testid='HotelSearchResult__sort__LOWEST_PRICE']")).click();
+		List<WebElement> prices = driver.findElement(By.xpath("//div[@data-testid='HotelSearchResult__ResultsList']")).findElements(By.className("Price__Value"));
+		int lowestP = Integer.parseInt(prices.get(0).getText());
+		int highestP = Integer.parseInt(prices.get(prices.size() - 1).getText());
+		assertEquals(lowestP < highestP, true);
 	}
 }
-
--- src/AlmosaferPKG/Parameters.java
+++ src/AlmosaferPKG/Parameters.java
@@ -1,11 +1,21 @@
 package AlmosaferPKG;
 
+import java.util.Random;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Parameters {
 	WebDriver driver = new ChromeDriver();
 	
-	String[] websites = { "https://global.almosafer.com/en", "https://global.almosafer.com/ar" };
+	Random rndCities = new Random();
+	
+	String[] websites = { "https://global.almosafer.com/ar", "https://global.almosafer.com/en" };
+	
+	String[] citiesEN = {"Dubai", "Jeddah", "Riyadh"};
+	int rndCitiesEN = rndCities.nextInt(citiesEN.length);
+	
+	String[] citiesAR = {"دبي","جدة"};
+	int rndCitiesAR = rndCities.nextInt(citiesAR.length);
 
 }
